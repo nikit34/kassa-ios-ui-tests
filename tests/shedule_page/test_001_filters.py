d6a158ebccd0795f38c9ce8c67104714b76072c4
n.permyakov <n.permyakov@rambler-co.ru>
2021-07-14 18:18:47 +0300

    ref

--- tests/shedule_page/test_001_filters.py
+++ tests/shedule_page/test_001_filters.py
@@ -62,26 +62,26 @@ class Test_001_ShedulePage:
                 self.shedule_page.check_rows_filters(dbg_api)
         finally:
             dbg_api.kill()
-    #
-    # def test_003_check_time_ticket_filter(self, driver):
-    #     """тапнуть на фичерс,
-    #     тапнуть на смотреть расписание,
-    #     проверять соответствие времени на билетах с выставленными фильтрами"""
-    #     self.shedule_page = ShedulePage(driver)
-    #     dbg_api = DebugAPI.run(request=False, mapi_handler=self.shedule_page.url_creations_movie_schedule_filter)
-    #     try:
-    #         with allure.step('MoviesPage'):
-    #             self.movie_page = MoviesPage(driver)
-    #             self.movie_page.set_custom_wait(10)
-    #             sleep(10)
-    #             self.movie_page.act.click_by_coords(50, 30)
-    #         with allure.step('EventDetailsPage'):
-    #             self.event_detail_page = MoviesDetailsPage(driver)
-    #             self.event_detail_page.set_custom_wait(10)
-    #             self.event_detail_page.click(*self.event_detail_page_locators.btn_view_timetable)
-    #         with allure.step('ShedulePage'):
-    #             self.shedule_page.set_custom_wait(10)
-    #             sleep(2)
-    #             self.shedule_page.compare_tickets_datetime_options_second_filter(dbg_api)
-    #     finally:
-    #         dbg_api.kill()
+
+    def test_003_check_time_ticket_filter(self, driver):
+        """тапнуть на фичерс,
+        тапнуть на смотреть расписание,
+        проверять соответствие времени на билетах с выставленными фильтрами"""
+        self.shedule_page = ShedulePage(driver)
+        dbg_api = DebugAPI.run(request=False, mapi_handler=self.shedule_page.url_creations_movie_schedule_filter)
+        try:
+            with allure.step('MoviesPage'):
+                self.movie_page = MoviesPage(driver)
+                self.movie_page.set_custom_wait(10)
+                sleep(10)
+                self.movie_page.act.click_by_coords(50, 30)
+            with allure.step('EventDetailsPage'):
+                self.event_detail_page = MoviesDetailsPage(driver)
+                self.event_detail_page.set_custom_wait(10)
+                self.event_detail_page.click(*self.event_detail_page_locators.btn_view_timetable)
+            with allure.step('ShedulePage'):
+                self.shedule_page.set_custom_wait(10)
+                sleep(2)
+                self.shedule_page.compare_tickets_datetime_options_second_filter(dbg_api)
+        finally:
+            dbg_api.kill()
